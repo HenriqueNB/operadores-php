2dc6b17089c03a21c57be408938a1fbf63f8ccf1
José Henrique Nascimento Bessa <josenb2345@gmail.com>
2026-09-17 11:52:51 -0300

    feat: add arithmetic operations calculator with PHP and CSS

---
+++ calcular.php
@@ -0,0 +1,58 @@
+<?php
+//Entrada
+$n1 = (float) $_POST['n1'];
+$n2 = (float) $_POST['n2'];
+
+//Processamento
+$adicao = $n1 + $n2;
+$subtracao = $n1 - $n2;
+$multiplicacao = $n1 * $n2;
+$divisao = $n1 / $n2;
+$modulo= $n1 % $n2;
+$potencia= $n1 ** $n2;
+$concatenacao = $n1.$n2; //O ponto junta o texto e, não soma como número
+
+//Saida
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações Aritiméticas</title>
+</head>
+<body>
+    <div class="card">
+        <h1 aling="center">Operações Aritiméticas</h1>
+        <h2 aling="center">Resultados</h2>
+        <hr>
+        <h3>
+            <?php echo "$n1 + $n2 = $adicao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 - $n2 = $subtracao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 * $n2 = $multiplicacao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 / $n2 = $divisao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 % $n2 = $modulo"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 ** $n2 = $potencia"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 . $n2 = $concatenacao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 <sup> $n2 </sup> = $potencia"; ?>
+        </h3>
+    </div>
+</body>
+</html>
